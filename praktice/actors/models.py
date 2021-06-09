0b957683d2c10b7ea09f895fa45ce96e9386ab7b
Student <student@lan.itstep.kiev.ua>
2021-06-09 20:02:02 +0300

    up

--- praktice/actors/models.py
+++ praktice/actors/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Actor(models.Model):
-    first_name = models.CharField(max_lengs=30)
+    first_name = models.CharField(max_length=30)
+    last_name =  models.CharField(max_length=40)
     
